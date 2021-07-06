6202fbd28547ab45ade51c1dc60eb699e594611f
MarketingPlus <benmarketingplus@gmail.com>
2021-07-06 10:46:21 +1000

    added the workouts api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,13 @@
 
 const db = require("../models");
 const router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
