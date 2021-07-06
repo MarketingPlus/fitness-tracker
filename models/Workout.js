8e06d037ed61c8ec70463e816dc09207c43c6fb9
MarketingPlus <benmarketingplus@gmail.com>
2021-07-06 10:52:25 +1000

    added a show virtuals graph for total duration and show exercise to the current total duration

--- models/Workout.js
+++ models/Workout.js
@@ -53,3 +53,9 @@ const WorkoutSchema = new Schema(
         }
     }
 );
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
