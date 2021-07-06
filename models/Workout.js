ae24a4b4e77d180abc1d86ea2f6e2bf177b0fa2a
MarketingPlus <benmarketingplus@gmail.com>
2021-07-06 10:51:36 +1000

    added set workout model

--- models/Workout.js
+++ models/Workout.js
@@ -1,2 +1,55 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter the type of exercise!"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter the name of the exercise!"
+                },
+                duration: {
+                    type: Number,
+                    trim: true,
+                    required: "Please enter the duration of your exercise!"
+                },
+                weight: {
+                    type: Number,
+                    trim: true,
+                    required: "Please enter the weight of your exercise!"
+                },
+                reps: {
+                    type: Number,
+                    trim: true,
+                    required: "Please enter the reps of your exercise!"
+                },
+                sets: {
+                    type: Number,
+                    trim: true,
+                    required: "Please enter the sets of your exercise!"
+                },
+                distance: {
+                    type: Number,
+                    trim: true,
+                    required: "Please enter the distance of your exercise!"
+                }
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
